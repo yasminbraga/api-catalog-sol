67e735ea22a70cfbd964769288ec73c0cf81e49b
yasminbraga <yasminbragateixeira@gmail.com>
2026-01-28 15:15:08 -0300

    feat:add register user controller

--- src/users/user.module.ts
+++ src/users/user.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 
@@ -6,5 +8,6 @@ import { UserService } from './user.service';
   controllers: [UserController],
   providers: [UserService],
   exports: [UserService],
+  imports: [TypeOrmModule.forFeature([User])],
 })
 export class UserModule {}
--- src/users/user.service.ts
+++ src/users/user.service.ts
@@ -1,6 +1,12 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { hash } from 'bcrypt';
 import { Repository } from 'typeorm';
+import { RequestUserDto } from './dto/request-user.dto';
 import { User } from './entities/user.entity';
 
 @Injectable()
@@ -8,6 +14,22 @@ export class UserService {
   constructor(
     @InjectRepository(User) private usersRepository: Repository<User>,
   ) {}
+
+  async register({ name, email, password }: RequestUserDto): Promise<User> {
+    const userExists = await this.usersRepository.findOneBy({ email });
+    if (userExists) {
+      throw new BadRequestException('User already exists');
+    }
+
+    const hashPassword = await hash(password, 10);
+    const newUser = this.usersRepository.create({
+      name,
+      email,
+      password: hashPassword,
+    });
+
+    return this.usersRepository.save(newUser);
+  }
   async findOne(email: string): Promise<User | undefined> {
     const user = await this.usersRepository.findOneBy({ email });
     if (!user) throw new NotFoundException('User not found');
